3f6c106bb48ded476fed280885399cbb27f8568f
melaniebalaz <melanie.j.balaz@gmail.com>
2026-03-23 12:25:19 +0100

    Rebuild portfolio site

--- intotech/README.md
+++
@@ -1,3 +0,0 @@
-# IntoTech-Info-Page
-
-A simple single page as a first information about the IntoTech conference
--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
-(function ($) {
-    'use strict';
+const navToggle = document.querySelector(".nav-toggle");
+const siteNav = document.querySelector(".site-nav");
+const workGroup = document.querySelector(".nav-work");
+const workTrigger = workGroup?.querySelector(":scope > a");
 
+if (navToggle && siteNav) {
+  navToggle.addEventListener("click", () => {
+    const expanded = navToggle.getAttribute("aria-expanded") === "true";
+    navToggle.setAttribute("aria-expanded", String(!expanded));
+    siteNav.classList.toggle("nav-visible");
+  });
+}
 
+if (workGroup && workTrigger) {
+  workTrigger.addEventListener("click", (event) => {
+    if (window.innerWidth <= 1100) {
+      event.preventDefault();
+      workGroup.classList.toggle("nav-open");
+      return;
+    }
 
-    // Sticky Menu
-    $(window).scroll(function () {
-        if ($('.navigation').offset().top > 100) {
-            $('.navigation').addClass('nav-bg');
-        } else {
-            $('.navigation').removeClass('nav-bg');
-        }
+    if (!workGroup.classList.contains("nav-open")) {
+      event.preventDefault();
+      workGroup.classList.add("nav-open");
+      return;
+    }
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!workGroup.contains(event.target)) {
+      workGroup.classList.remove("nav-open");
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      workGroup.classList.remove("nav-open");
+    }
+  });
+
+  workGroup.querySelectorAll(".dropdown-link").forEach((link) => {
+    link.addEventListener("click", () => {
+      workGroup.classList.remove("nav-open");
     });
+  });
+}
 
-    // Background-images
-    $('[data-background]').each(function () {
-        $(this).css({
-            'background-image': 'url(' + $(this).data('background') + ')'
-        });
+if (siteNav) {
+  siteNav.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      if (window.innerWidth <= 1100) {
+        siteNav.classList.remove("nav-visible");
+        navToggle?.setAttribute("aria-expanded", "false");
+      }
     });
-
-    // background color
-    $('[data-color]').each(function () {
-        $(this).css({
-            'background-color': $(this).data('color')
-        });
-    });
-
-    // progress bar
-    $('[data-progress]').each(function () {
-        $(this).css({
-            'bottom': $(this).data('progress')
-        });
-    });
-
-    /* ########################################### /hero parallax ############################################## */
-
-
-    // Shuffle js filter and masonry
-    var Shuffle = window.Shuffle;
-    var jQuery = window.jQuery;
-
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-        itemSelector: '.shuffle-item',
-        buffer: 1
-    });
-
-    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-        var input = evt.currentTarget;
-        if (input.checked) {
-            myShuffle.filter(input.value);
-        }
-    });
-
-
-
-})(jQuery);
+  });
+}
